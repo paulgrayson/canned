a0408728623f10670e01dfae6a256a5d636a4f1d
Paul Grayson <paul@graysontechnology.co.uk>
2012-04-09 16:17:44 +0100

    get express working

--- app.js
+++ app.js
@@ -3,42 +3,34 @@
  * Module dependencies.
  */
 
-var express = require('express');
+var express = require('express')
+  , routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.use(express.bodyDecoder());
-    app.use(express.methodOverride());
-    app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
-    app.use(app.router);
-    app.use(express.staticProvider(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-   app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
 
-app.get('/', function(req, res){
-    res.render('index.jade', {
-        locals: {
-            title: 'Express'
-        }
-    });
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
-
-// Only listen on $ node app.js
-
-if (!module.parent) {
-    app.listen(3000);
-    console.log("Express server listening on port %d", app.address().port)
-}
