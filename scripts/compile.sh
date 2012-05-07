f49c58e7fb2837bdf41f84298480ca28593e3d10
Paul Grayson <paul.grayson@gmail.com>
2012-05-07 14:40:21 +0100

    compile script kills any watching compile processes - not ideal but better

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,4 @@
+ps -Af | awk '/bin\/coffee/ { print "kill "$2 }' | sh
 coffee -w -c -j lib/canned-server.js src/server/*.cs &
 coffee -w -c -j public/javascripts/canned-client.js src/client/*.cs &
 
