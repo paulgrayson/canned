d838557c33e6e9d4f35b962bf1478dfea1dc0cd0
Paul Grayson <paul.grayson@graysontechnology.co.uk>
2014-03-19 15:16:37 +0000

    Remove compile script as it's been replaced with gulp

--- scripts/compile.sh
+++
@@ -1,5 +0,0 @@
-ps -Af | awk '/bin\/coffee/ { print "kill "$2 }' | sh
-coffee -w -c -j lib/canned-server.js src/server/*.cs &
-coffee -w -c -j public/javascripts/canned-client.js src/client/*.cs &
-
-
