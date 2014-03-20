9dfde4f8f326e5007aade417ec398578211bdb51
Paul Grayson <paul.grayson@graysontechnology.co.uk>
2014-03-20 16:41:43 +0000

    Remove some unnecessary logging

--- src/server/sockets.cs
+++ src/server/sockets.cs
@@ -5,18 +5,14 @@ mongoConnect ( err, db )->
       console.log( "connected" )
 
       socket.on 'login', ( userid )->
-        console.log( "Login: #{userid}" )
         fetchOrCreateUserColor db, userid, ( err, color )->
           if err
             logError( err )
           else
-            console.log( "color "+ color )
-
             fetchChat db, ( err, chat )->
               if err
                 logError( err )
               else
-                console.log( chat )
                 socket.emit("welcome", {
                   userid: userid
                   color: color,
@@ -25,13 +21,11 @@ mongoConnect ( err, db )->
                 socket.broadcast.emit('joined', {userid: userid, color: color})
 
       socket.on 'chat', ( data )->
-        console.log(data)
         socket.broadcast.emit('chat', data)
         addMessage db, data.color, data.userid, data.text, ( err, docs )->
           console.log( "wrote #{docs}" )
       
       socket.on 'typed', ( data )->
-        console.log( data )
         socket.broadcast.emit('typed', data)
   }
 
